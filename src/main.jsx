bafe19269e195be6681f2414c8126e081ca3859b
amirsalar <salar.norouzi80@gmail.com>
2025-07-30 13:12:10 +0330

    file -9

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,48 @@
-import { StrictMode } from 'react'
+import React,{ StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./app.css"
 
-createRoot(document.getElementById('root')).render(<h1>hi</h1>)
+class Hi extends React.Component{
+  render(){
+    return(
+      <h1>hi</h1>
+    )
+  }
+}
+class Timer extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      time: new Date().toLocaleTimeString()
+    }
+  }
+  render(){
+    setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleTimeString()
+      }
+      )
+      
+    },1000);
+    return(
+      <h2 className='timer'>
+          it is {this.state.time}
+      </h2>
+    )
+  }
+}
+class Test extends React.Component{
+  render(){
+    return (
+      <div className='main'>
+        <Hi/>
+        <Timer/>
+      </div>
+    )
+  }
+}
+
+
+const root=createRoot(document.getElementById('root'))
+
+ root.render(<Test/>);
